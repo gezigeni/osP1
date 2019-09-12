acc84fb74c25044189c528204d7ff19330b636b1
omer muhit <muhit@hoare7.cs.umsl.edu>
2019-09-12 12:55:26 -0500

    last one

--- dt.c
+++ dt.c
@@ -1,73 +1,146 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <limits.h>
-void depthFirst(DIR *dir){
-        struct dirent *sd;
-        char path[PATH_MAX];         
-                
+#include <time.h>
 
+//indentation is set to a static global variable. It is 4 as default. 
+static int indentation = 4;
+static int numberlink;
+
+//this function prints last accessed time modification of a file
+void printaccess(char *path) {
+	struct stat statbuf;
+	if (stat(path, &statbuf) == -1)
+      		perror("Failed to get file status");
+	else
+		printf("%s last accessed at %s", path, ctime(&statbuf.st_atime));
+}
+//depth first function 
+void depthFirst(char * path){
+        struct dirent *sd;
+	DIR *dir;
+        char rpath[PATH_MAX];
+	dir = opendir(path);
+        int i;        
+	struct stat sb;
+		//if the directory can not be opened it returns null and terminates the program with exit(1) code
                 if(dir == NULL){
                         printf("Error, unable to open\n");
                         exit(1);
                 }
-
+		//it loops until it reads the last directory in the current directory, returns null when it reaches to end
                 while( (sd = readdir(dir)) != NULL){
+			//ignoring .  ,  .. 
 			if(strcmp(sd->d_name, ".") != 0 && strcmp(sd->d_name, "..") != 0){
-                        printf("%s\n", sd->d_name);
-                	realpath(sd->d_name,path);
-			if(isdirectory(path)){
-				printf("\t");
-				depthFirst(opendir(sd->d_name));
+			//prints a space in the amount of static variable indentation
+			for(i = 0; i < indentation; i++){
+                                       printf(" ");
+                        }
+			
+			printf("%s\n",sd->d_name);
+			realpath(sd->d_name,rpath);
+		
+			
+	
+			if(isdirectory(rpath)){
+				depthFirst(sd->d_name);
+			
+				
 			}		
+			                       
 		}
 		        
                 }
-                
 
+        
+	closedir(dir);
 }
+int groupId(char *path) {
+   struct stat statbuf;
+   if (stat(path, &statbuf) == -1){
+        perror("stat");
+   }
+        else
+        return statbuf.st_gid; 
+}
+int userId(char *path) {
+   struct stat statbuf;
+   if (stat(path, &statbuf) == -1){
+        perror("stat");
+   }
+        else
+        return statbuf.st_uid;
+}
+int numberLinks(char *path) {
+   struct stat statbuf;
+   if (stat(path, &statbuf) == -1){
+	perror("stat");
+      return 0;
+   }
+        else
+        return S_ISLNK(statbuf.st_nlink); }
 int isdirectory(char *path) {
    struct stat statbuf;
-   if (stat(path, &statbuf) == -1)
+   if (stat(path, &statbuf) == -1){
+	perror("stat");
       return 0;
+   }
 	else
 	return S_ISDIR(statbuf.st_mode); }
 int main(int argc, char *argv[]){
-
-	
+	char * path;
+	char * currentDirectory = ".";
+	int i;
+	int userid,groupid;
 
 	if(argc<2){
 		printf("No arguments");
-		DIR *dir;
-		dir = opendir(".");
-		depthFirst(dir);	
-		closedir(dir);
+		//currentDirectory = ".";
+		depthFirst(currentDirectory);
 	}
-	else{
+	else{                
+		path = argv[1];
+	
+
 		int c;
-		while( (c = getopt(argc, argv, "hILdgipstul")) != -1){
+		while( (c = getopt(argc, argv, "hI:Ldgipstul")) != -1){
+		
 			switch(c){
 				case 'h':
+					printf("This is the help message, please read README file for further information");
+					exit(1);
 					printf("In the help page\n");
 					break;
 				case 'I':
                                         printf("Setting indentation\n");
+					indentation = atoi(optarg);
+					printf("Indentation is: %d\n", indentation);
                                         break;
 				 case 'L':
                                         printf("Following symbolic links\n");
                                         break;
 				 case 'd':
-                                        printf("Time of last modification\n");
-                                        break;
+                                        //printf("Time of last modification\n");
+                                        if(argc<2)
+						printaccess(currentDirectory);
+                                       	else
+						printaccess(path);
+					break;
 				 case 'g':
-                                        printf("Print group id\n");
-                                        break;
+                                        //printf("Print group id\n");
+					groupid = groupId(path);
+                                        printf("Group Id is: %d\n",groupid);
+					break;
 				 case 'i':
                                         printf("Print number of links in inode table\n");
-                                        break;
+					int numberlink = numberLinks(path);
+                        		printf("number of links: %d\n",numberlink);
+					 break;
 				 case 'p':
                                         printf("Permissions\n");
                                         break;
@@ -78,14 +151,15 @@ int main(int argc, char *argv[]){
                                         printf("Information of file\n");
                                         break;
 				 case 'u':
-                                        printf("Print user id\n");
+                                        //printf("Print user id\n");
+					userid = userId(path);
+                                        printf("User Id is: %d\n",userid);
                                         break;
 				 case 'l':
                                         printf("Optional one\n");
-                                        break;
+                                        break;	
 				default:
 					perror("Not a valid command-line argument");
-					printf("Not valid");
 					break;
 
 
@@ -97,14 +171,21 @@ int main(int argc, char *argv[]){
 
 
 		}
-
-
+		
+		if (optind < argc) {
+			path = argv[optind];
+			//printf(path);
+			depthFirst(path);
+		}else 
+			depthFirst(currentDirectory);
+	}				
+		//depthFirst(path);
+		return 0;
 	}
 
 
 
 
 
-	return 0;
-}
+
 
